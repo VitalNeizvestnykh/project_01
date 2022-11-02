2f1547c61711e6dcff4ee3ebedab536502febd7c
VitalNeizvestnykh <115677941+VitalNeizvestnykh@users.noreply.github.com>
2022-11-02 12:56:34 +0300

    Add files via upload

---
+++ task_01.py
@@ -0,0 +1,10 @@
+# Задача 1
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+songs = my_favorite_songs.split(', ')
+
+print(songs[0], songs[-1], songs[1], songs[-2])
